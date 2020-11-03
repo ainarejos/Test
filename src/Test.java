ffea78a5625a079706755d6b9bdbbd5f1f1eb020
ainarejos <ainarejos@nigul.cide.es>
2020-11-03 18:42:03 +0100

    Se añaden comentarios al codigo.

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
+        // se definen las variables y una array.
         int a, b;
         double c;
         double[] array1=new double[10];
+        //se crea el prgrama scanner para pedir los valores de a y b por tecaldo.
         Scanner teclado=new Scanner(System.in);
         System.out.print("Introduce el valor de a: ");
         a=teclado.nextInt();
@@ -11,8 +13,11 @@ public class Test {
         b=teclado.nextInt();
         System.out.print("Introduce el valor de 0 de array1: ");
         array1[0]=teclado.nextDouble();
+        //se define un valor sum y se etablece la suma de a + b como su valor.
         int sum=a+b;
+        //cast
         c=(double)a;
+        //Se imprime por pantalla los resultados del programa.
         System.out.println("La suma de a + b es: " + sum);
         System.out.println("El primer valor de la array1 es:" + array1[0]);
         System.out.println("EL valor de c es: " + c);
